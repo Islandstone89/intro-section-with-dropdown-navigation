b4ab7b40f9ad2a60d2f1c13344403c91a1b78138
Øystein Håberg <islandstone89@gmail.com>
2024-06-15 23:59:39 +0200

    * Added show and hide styles for ioen menu and close menu * Created function for closeing mobile menu

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+/* SELECTORS */
+
 /* Select first nav button */
 let navigationButton1 = document.getElementById("first-button");
 
@@ -12,6 +14,7 @@ let openMenuButton = document.querySelector(".js-open-menu");
 let closeMenuButton = document.querySelector(".js-close-menu");
 
 
+/* FUNCTIONS */
 
 /* Create function that toggles the first submenu */
 function toggleFirstSubmenu () {
@@ -23,10 +26,6 @@ function toggleFirstSubmenu () {
     }
 }
 
-/* Add Event Listener on first nav button */
-navigationButton1.addEventListener("click", toggleFirstSubmenu);
-
-
 /* Create function that toggles the second submenu */
 function toggleSecondSubmenu () {
     if (navigationButton2.ariaExpanded === "false") {
@@ -37,14 +36,29 @@ function toggleSecondSubmenu () {
     }
 }
 
-/* Add Event Listener on second nav button */
-navigationButton2.addEventListener("click", toggleSecondSubmenu);
-
 
 /* Create function that opens mobile menu */
 function openMenu() {
     openMenuButton.setAttribute("aria-expanded", "true");
 }
 
+/* Create function that closes mobile menu */
+function closeMenu() {
+    openMenuButton.setAttribute("aria-expanded", "false")
+}
+
+
+/* EVENT LISTENERS */
+
+/* Add Event Listener on first nav button */
+navigationButton1.addEventListener("click", toggleFirstSubmenu);
+
+/* Add Event Listener on second nav button */
+navigationButton2.addEventListener("click", toggleSecondSubmenu);
+
+
 /* Add Event Listener on open menu button */
 openMenuButton.addEventListener("click", openMenu);
+
+/* Add Event Listener on close menu button */
+closeMenuButton.addEventListener("click", closeMenu);
